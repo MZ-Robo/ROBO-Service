69cef508e046261b4e14bec44db6153c2764c01e
miffDONG <ksglsg1350@gmail.com>
2023-09-04 10:59:01 +0900

    23-09-03 work - page.home

--- front/lib/home.dart
+++ front/lib/home.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'signup.dart';
+import 'package:front/lowbar.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,15 +10,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Widget _whatIs() {
-    return Container(
-      width: 203,
-      height: 46,
-      margin: EdgeInsets.fromLTRB(40, 10, 0, 10),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
+  final double lowbarHeight = 86;
+
+  Widget _whatIs(String text) {
+    return IntrinsicWidth(
+      child: Container(
+        height: 46,
+        margin: EdgeInsets.fromLTRB(40, 10, 0, 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        child: Center(
+          child: Text(text),
         ),
       ),
     );
@@ -48,179 +56,275 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  Widget _weather(String imgUrl, String temperature, String textTemperature,
+      String date, String percentage) {
+    return Container(
+      child: Stack(children: [
+        //날씨 background
+        Positioned(
+          child: Container(
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 71, 0, 0),
+                width: 320,
+                height: 170,
+                decoration: ShapeDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment(0.94, -0.34),
+                    end: Alignment(-0.94, 0.34),
+                    colors: [Color(0xFF67E1D2), Color(0xFF54A8FF)],
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        //날씨 asset
+        Positioned(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Image.asset(
+              imgUrl,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        //날씨 온도
+        Positioned(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(227, 91, 0, 0),
+            child: Text(
+              temperature,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 65,
+                // fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+                height: 1.20,
+              ),
+            ),
+          ),
+        ),
+        //투자 날씨
+        Positioned(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(60, 180, 0, 0),
+            child: Text(
+              textTemperature,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontFamily: 'pretendard',
+                fontWeight: FontWeight.w700,
+                height: 1.20,
+              ),
+            ),
+          ),
+        ),
+        //날짜
+        Positioned(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(240, 180, 0, 0),
+            child: Text(
+              date,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontFamily: 'pretendard',
+                fontWeight: FontWeight.w600,
+                height: 1.20,
+              ),
+            ),
+          ),
+        ),
+        //상승률
+        Positioned(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(280, 200, 0, 0),
+            child: Text(
+              percentage,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontFamily: 'pretendard',
+                fontWeight: FontWeight.w600,
+                height: 1.20,
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget _SunriseSunset() {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.fromLTRB(0, 578, 0, 0),
+        width: 320,
+        height: 237,
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.only(left: 21, top: 16),
+                child: Text(
+                  '일출일몰',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.only(left: 21, top: 35),
+                child: Text(
+                  '내 투자에 볕들날은 언제올까?',
+                  style: TextStyle(
+                    color: Color(0xFF727272),
+                    fontSize: 8,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.only(top: 198),
+                child: Center(
+                  child: Text(
+                    '투자 목표금액 40만원까지 40퍼센트 남았어요.',
+                    style: TextStyle(
+                      color: Color(0xFF727272),
+                      fontSize: 9,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(children: [
-          Expanded(
-              child: Container(
-            height: 1283,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.00, -1.00),
-                end: Alignment(0, 1),
-                colors: [Color(0xFF4F3993), Color(0xFF14113C)],
-              ),
+        body: Column(
+          children: [
+            // topBar,
+            Container(
+              height: 20,
+              color: Color(0xFF4F3993),
             ),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(height: 49),
-                  Positioned(
-                      child: Stack(
+            Expanded(
+              child: Container(
+                // height: 1283,
+                // height: MediaQuery.of(context).size.height - lowbarHeight,
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment(0.00, -1.00),
+                    end: Alignment(0, 1),
+                    colors: [Color(0xFF4F3993), Color(0xFF14113C)],
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      Container(
-                        width: 24,
-                        height: 24,
-                        margin: EdgeInsets.fromLTRB(291, 9, 0, 0),
-                        child: Image.asset(
-                          'assets/bell.png',
-                          fit: BoxFit.fill,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Container(
-                        width: 35,
-                        height: 35,
-                        margin: EdgeInsets.fromLTRB(321, 9, 0, 0),
-                        child: Image.asset(
-                          'assets/justify.png',
-                          fit: BoxFit.fill,
-                          color: Colors.white,
-                        ),
-                      ),
-                      //날씨 background
+                      // const SizedBox(height: 49),
                       Positioned(
-                        child: Container(
-                          child: Center(
-                            child: Container(
-                              margin: EdgeInsets.fromLTRB(0, 71, 0, 0),
-                              width: 320,
-                              height: 170,
-                              decoration: ShapeDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment(0.94, -0.34),
-                                  end: Alignment(-0.94, 0.34),
-                                  colors: [
-                                    Color(0xFF67E1D2),
-                                    Color(0xFF54A8FF)
-                                  ],
-                                ),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(25),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      //날씨 asset
-                      Positioned(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                          child: Image.asset(
-                            'assets/cloud.png',
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                      //날씨 온도
-                      Positioned(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(227, 91, 0, 0),
-                          child: Text(
-                            '27。',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 65,
-                              // fontFamily: 'Inter',
-                              fontWeight: FontWeight.w700,
-                              height: 1.20,
-                            ),
-                          ),
-                        ),
-                      ),
-                      //투자 날씨
-                      Positioned(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(60, 180, 0, 0),
-                          child: Text(
-                            '투자 온도 \n맑음',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontFamily: 'pretendard',
-                              fontWeight: FontWeight.w700,
-                              height: 1.20,
-                            ),
-                          ),
-                        ),
-                      ),
-                      //날짜
-                      Positioned(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(240, 180, 0, 0),
-                          child: Text(
-                            '2023/08/20',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontFamily: 'pretendard',
-                              fontWeight: FontWeight.w600,
-                              height: 1.20,
-                            ),
-                          ),
-                        ),
-                      ),
-                      //상승률
-                      Positioned(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(280, 200, 0, 0),
-                          child: Text(
-                            '2.37%',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontFamily: 'pretendard',
-                              fontWeight: FontWeight.w600,
-                              height: 1.20,
-                            ),
-                          ),
-                        ),
-                      ),
-
-                      Positioned(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Stack(
                           children: [
                             Container(
-                              child: Container(
-                                margin: EdgeInsets.fromLTRB(0, 250, 0, 0),
+                              width: 24,
+                              height: 24,
+                              margin: EdgeInsets.fromLTRB(291, 12, 0, 0),
+                              child: Image.asset(
+                                'assets/bell.png',
+                                fit: BoxFit.fill,
+                                color: Colors.white,
                               ),
                             ),
-                            _whatIs(),
-                            _whatIs(),
-                            _whatIs(),
-                            _whatIs(),
+                            Container(
+                              width: 35,
+                              height: 35,
+                              margin: EdgeInsets.fromLTRB(321, 9, 0, 0),
+                              child: Image.asset(
+                                'assets/justify.png',
+                                fit: BoxFit.fill,
+                                color: Colors.white,
+                              ),
+                            ),
+                            //투자 온도
+                            _weather('assets/cloud.png', '27。', '투자 온도 \n맑음',
+                                '2023/08/20', '2.37%'),
+                            //투자온도 아래 항목들
+                            Positioned(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    child: Container(
+                                      margin: EdgeInsets.fromLTRB(0, 250, 0, 0),
+                                    ),
+                                  ),
+                                  _whatIs('12341234'),
+                                  _whatIs('asdfqwerzxcv'),
+                                  _whatIs('1234'),
+                                  _whatIs('123512341235asdf'),
+                                ],
+                              ),
+                            ),
+                            //추가 버튼
+                            Positioned(
+                              child: Container(
+                                margin: EdgeInsets.fromLTRB(0, 520, 0, 0),
+                                child: Center(child: _plusCircle()),
+                              ),
+                            ),
+                            _SunriseSunset(),
                           ],
                         ),
                       ),
-                      Positioned(
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(0, 520, 0, 0),
-                          child: Center(child: _plusCircle()),
-                        ),
-                      ),
                     ],
-                  )),
-                ],
+                  ),
+                ),
               ),
             ),
-          ))
-        ]),
+            // Container(
+            //   height: 85,
+            //   child: Row(
+            //     children: [
+            //       Container(),
+            //       Container(),
+            //       Container(),
+            //     ],
+            //   ),
+            // ),
+            LowBarUnit(),
+          ],
+        ),
       ),
     );
   }
---
+++ front/lib/lowbar.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:front/home.dart';
+
+class LowBarUnit extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 85,
+      child: Row(
+        children: [
+          SizedBox(width: 85),
+          SizedBox(
+              width: 24,
+              height: 52,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                },
+                child: Column(children: [
+                  Container(height: 2),
+                  Positioned(
+                      child: Image.asset(
+                    'assets/home.png',
+                    width: 24,
+                    height: 24,
+                  )),
+                  Container(
+                    height: 6,
+                  ),
+                  Positioned(
+                    child: Text(
+                      '홈',
+                      style: TextStyle(
+                        // color: Color(0xFF4F3993),
+                        fontSize: 14,
+                        fontFamily: 'Noto Sans KR',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ]),
+              )),
+          SizedBox(width: 65),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            width: 32,
+            height: 50,
+            child: InkWell(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => HomeScreen()),
+                // );
+              },
+              child: Column(children: [
+                Positioned(
+                    child: Image.asset(
+                  'assets/diagnosis.png',
+                  width: 28,
+                  // height: 28,
+                )),
+                Positioned(
+                  child: Text(
+                    '진단',
+                    style: TextStyle(
+                      // color: Color(0xFF4F3993),
+                      fontSize: 14,
+                      fontFamily: 'Noto Sans KR',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+          SizedBox(width: 60),
+          SizedBox(
+            width: 52,
+            height: 55,
+            child: InkWell(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => HomeScreen()),
+                // );
+              },
+              child: Column(children: [
+                Container(
+                  height: 5,
+                ),
+                Positioned(
+                    child: Image.asset(
+                  'assets/checkbox.png',
+                  width: 24,
+                  // height: 24,
+                )),
+                Container(
+                  height: 4,
+                ),
+                Positioned(
+                  child: Text(
+                    '둘러보기',
+                    style: TextStyle(
+                      // color: Color(0xFF4F3993),
+                      fontSize: 14,
+                      fontFamily: 'Noto Sans KR',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
